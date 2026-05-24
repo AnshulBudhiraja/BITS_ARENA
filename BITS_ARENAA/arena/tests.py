5bd9c3262243699e642c30aa61c1dd8e164ad71f
AnshulBudhiraja <anshulbudhiraja@example.com>
2026-05-25 01:23:03 +0530

    Upgrade pipeline to build Docker and run Django tests

--- BITS_ARENAA/arena/tests.py
+++ BITS_ARENAA/arena/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SanityCheckTest(TestCase):
+    def test_environment_is_working(self):
+        # If Django is running correctly, 1 + 1 will equal 2
+        self.assertEqual(1 + 1, 2)
